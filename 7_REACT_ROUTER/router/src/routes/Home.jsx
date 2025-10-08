d7cded2adce160a17d9be8aceff49c8df4c9a053
Patrick Corrêa <patrickcorrea250@gmail.com>
2025-10-07 21:22:09 -0300

    carregando_dados

--- 7_REACT_ROUTER/router/src/routes/Home.jsx
+++ 7_REACT_ROUTER/router/src/routes/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+// 6 - carregamento de dados
+import {useFetch} from '../hooks/useFetch'
+
+const url = "http://localhost:3000/products";
+
 const Home = () => {
+
+  const {data: items} = useFetch(url)
+
+
+
   return (
     <div>
         <h1>Home</h1>
+        {/* 6 - carregando dados */}
+        <ul className="products">
+          {items && items.map((item) => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>R$: {item.price}</p>
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
