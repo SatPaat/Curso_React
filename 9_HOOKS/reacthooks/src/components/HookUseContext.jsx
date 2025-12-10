22a0b9c066dfdd31cb8030d5b8b6cb805fc83fe3
Patrick Corrêa <patrickcorrea250@gmail.com>
2025-12-10 20:32:57 -0300

    useContext_commit

---
+++ 9_HOOKS/reacthooks/src/components/HookUseContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react"
+
+export const someContext = createContext()
+
+
+
+export const HookUseContext = ({ children }) => {
+
+    const contextValue = "testing context"
+
+    return (
+        <SomeContext.Provider value={{contextValue}}>
+            {children}
+        </SomeContext.Provider>
+    )
+}
+
+export default HookUseContext
